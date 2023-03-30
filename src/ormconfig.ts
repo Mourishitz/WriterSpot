3b9c9a655b0d9e0e010e733454b6d15b6883289c
Mourishitz <gabrielmorishita@hotmail.com>
2023-03-30 16:57:08 -0300

    ✨ feat: improving cli commands

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,8 @@ const config: PostgresConnectionOptions = {
     password: 'postgres',
     database: 'nest',
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    synchronize: true,
+    synchronize: false,
+    migrations: [__dirname + '/migrations/**/*.{.ts,.js}']
 };
 
 export default config;
