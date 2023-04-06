fe9848b7d631a08e55b09d2da00a7ad7942f403c
Mourishitz <gabrielmorishita@hotmail.com>
2023-04-06 07:53:56 -0300

    ✨ feat: finishing database interaction with users and tags

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "@app/user/user.service";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
-import { UserEntity } from "@app/user/user.entity";
+import { UserResponseInterface } from "./types/userResponse.interface";
 
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
     @Post()
-    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserEntity> {
-        return this.userService.createUser(createUserDto);
+    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+        const user = await this.userService.createUser(createUserDto);
+        return this.userService.buildUserResponse(user);
     }
 }
