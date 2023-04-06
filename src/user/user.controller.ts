cff49040f784603bea244284951e42d9c2c22015
Mourishitz <gabrielmorishita@hotmail.com>
2023-04-06 09:17:02 -0300

    🛂 feat: creating auth guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,30 +1,36 @@
-import { Body, Controller, Get, Post, Req, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UserService } from "@app/user/user.service";
 import { CreateUserDto } from "@app/user/dto/createUser.dto";
 import { UserResponseInterface } from "@app/user/types/userResponse.interface";
 import { LoginUserDto } from "@app/user/dto/loginUser.dto";
-import { Request } from "express";
-import { ExpressRequest } from "@app/types/expressRequest.interface";
+import { User } from "@app/user/decorators/user.decorator";
+import { AuthGuard } from "@app/user/guards/auth.guard";
+import { UserEntity } from "./user.entity";
 
-@Controller('users')
+@Controller()
 export class UserController {
     constructor(private readonly userService: UserService) {}
 
-    @Post()
+    @Post('users')
     @UsePipes(new ValidationPipe())
-    async createUser(@Body('user') createUserDto: CreateUserDto): Promise<UserResponseInterface> {
+    async createUser(
+        @Body('user') createUserDto: CreateUserDto
+    ): Promise<UserResponseInterface> {
         const user = await this.userService.createUser(createUserDto);
         return this.userService.buildUserResponse(user);
     }
 
-    @Get()
-    async currentUser(@Req() request: ExpressRequest): Promise<UserResponseInterface> {
-        return this.userService.buildUserResponse(request.user);
+    @Get('user')
+    @UseGuards(AuthGuard)
+    async currentUser(@User() user: UserEntity): Promise<UserResponseInterface> {
+        return this.userService.buildUserResponse(user);
     }
 
-    @Post('login')
+    @Post('users/login')
     @UsePipes(new ValidationPipe())
-    async login(@Body('user') loginDto: LoginUserDto): Promise<UserResponseInterface> {
+    async login(
+        @Body('user') loginDto: LoginUserDto
+    ): Promise<UserResponseInterface> {
         const user = await this.userService.login(loginDto)
         return this.userService.buildUserResponse(user)
     }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,7 +2,8 @@ import { Module } from "@nestjs/common";
 import { UserController } from "@app/user/user.controller";
 import { UserService } from "@app/user/user.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntity } from "./user.entity";
+import { UserEntity } from "@app/user/user.entity";
+import { AuthGuard } from "@app/user/guards/auth.guard";
 
 @Module({
     imports: [
@@ -11,7 +12,7 @@ import { UserEntity } from "./user.entity";
         )
     ],
     controllers: [UserController],
-    providers: [UserService],
+    providers: [UserService, AuthGuard],
     exports: [UserService]
 })
 export class UserModule {};
